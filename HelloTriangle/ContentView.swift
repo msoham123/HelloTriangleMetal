45310e034aabce7a53be0c8d1d93a1e4922a8023
msoham123 <msoham123@gmail.com>
2021-11-26 18:53:49 -0800

    Initial commit

--- HelloTriangle/ContentView.swift
+++ HelloTriangle/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        MetalView().frame(width: CGFloat(500), height: CGFloat(500), alignment: .center)
     }
 }
 
---
+++ HelloTriangle/MetalView.swift
@@ -0,0 +1,73 @@
+//
+//  MetalView.swift
+//  HelloTriangle
+//
+//  Created by Soham Manoli on 11/26/21.
+//
+
+
+import SwiftUI
+import MetalKit
+
+struct MetalView: NSViewRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func updateNSView(_ nsView: MTKView, context: NSViewRepresentableContext<MetalView>) {
+        
+    }
+    
+    func makeNSView(context: NSViewRepresentableContext<MetalView>) -> MTKView {
+        
+        let metalView = MTKView()
+        
+        if let metalDevice = MTLCreateSystemDefaultDevice() {
+            metalView.device = metalDevice
+        }
+        else {
+            fatalError("[!] No appropriate Metal Devices found.")
+        }
+        
+        metalView.framebufferOnly = false
+        metalView.enableSetNeedsDisplay = false
+        metalView.delegate = context.coordinator
+        metalView.clearColor = MTLClearColorMake(0.0, 1.0, 0.0, 1.0)
+        
+        return metalView
+    }
+    
+    class Coordinator : NSObject, MTKViewDelegate {
+        
+        var parent: MetalView
+        
+        init(_ parent: MetalView) {
+            self.parent = parent
+            super.init()
+        }
+        
+        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+            
+        }
+        
+        func draw(in view: MTKView) {
+
+            // Gives you access to drawable space in your window, useful for rendering pipeline
+            guard let drawable = view.currentDrawable else {
+                return
+            }
+            
+            // Rendering commands go here
+            // Executed every frame
+            
+        }
+    }
+}
+
+struct MetalView_Previews: PreviewProvider {
+    static var previews: some View {
+        MetalView()
+    }
+}
+
