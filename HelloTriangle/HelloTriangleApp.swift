46c24cc6edfbca7a039332a8282e12095a4b4e3e
msoham123 <msoham123@gmail.com>
2021-11-26 18:45:15 -0800

    Initial Commit

---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by Soham Manoli on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
