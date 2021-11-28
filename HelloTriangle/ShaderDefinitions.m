5edf37258446bcc0e8b37143f0b8c184a504f110
msoham123 <msoham123@gmail.com>
2021-11-28 15:13:10 -0800

    Create Obj-C bridge file for Swift to MSL

---
+++ HelloTriangle/ShaderDefinitions.m
@@ -0,0 +1,14 @@
+//
+//  ShaderDefinitions.m
+//  HelloTriangle
+//
+//  Created by Soham Manoli on 11/28/21.
+//
+
+#import <Foundation/Foundation.h>
+#include <simd/simd.h>
+
+struct Vertex {
+    vector_float4 color;
+    vector_float2 pos;
+};
